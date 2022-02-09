43f3f8873022c9d116b2040396d15fd26df11afe
Nakio82 <ignaciogalasso82@gmail.com>
2022-02-08 22:10:29 -0300

    new project To-Do List

---
+++ docs/js_projects/js/tip.js
@@ -0,0 +1,33 @@
+/* TIP GENERATOR */
+// Calling the elements
+const money = document.getElementById('money');
+const tip = document.getElementById('tip');
+const diners = document.getElementById('diners');
+const enterBtn = document.querySelector('.enter-btn');
+const amout = document.getElementById('amout');
+const tipValue = document.getElementById('tip-value');
+const total = document.getElementById('total');
+const pay = document.getElementById('pay');
+const display = document.querySelector('.display');
+
+
+// Operating button
+enterBtn.addEventListener('click', () => {
+    
+    amout.textContent = `Ticket: ${money.value}`;
+    
+    let totalTip = (money.value * tip.value) / 100;
+    tipValue.textContent = `Tip: ${totalTip}`;
+    
+    let totalEach = parseInt(totalTip) + parseInt(money.value);
+    total.textContent = `Ticket + Tip: ${totalEach}`;
+    
+    let totalPay =  totalEach / diners.value;
+    pay.textContent = `Total Each: ${totalPay.toFixed(2)}`;
+    pay.classList.add('pay');
+    // console.log(totalPay)
+    money.value = '';
+    tip.value = '';
+    diners.value = '';
+
+})
