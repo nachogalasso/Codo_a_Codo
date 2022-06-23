e006ddacad9aa96cecb0e8b95eeaae6090321ec7
nachogalasso <ignaciogalasso82@gmail.com>
2022-06-23 18:41:27 +0200

    add JS features

--- docs/gamecard/script.js
+++ docs/gamecard/script.js
@@ -4,6 +4,58 @@ const visitor = document.getElementById("visitorHeader");
 const homeCard = document.querySelector(".hometeam");
 const visitorCard = document.querySelector(".visitorteam");
 const viewAll = document.querySelector(".show_cards");
+const form = document.querySelector(".card_form");
+
+// MODAL
+const modal = document.getElementById("myModal");
+const closeModal = document.querySelector(".close");
+const emailValid = document.getElementById("card_email");
+const modalEmail = document.querySelector(".email_data");
+
+const emailIsSet = {
+  email: false,
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(function () {
+    modal.classList.add("show_modal");
+  }, 2000);
+})
+
+// closeModal.addEventListener('Click', (e) => {
+//   if(e.target == modal) {
+//     modal.classList.remove("show_modal");
+//   }
+// })
+
+window.onclick = function(e) {
+  if (e.target == modal) {
+    modal.classList.remove("show_modal");
+  }
+}
+
+modalEmail.addEventListener('submit', (e) => {
+  e.preventDefault();
+  validateForm();
+})
+
+emailValid.addEventListener('change', (e) => {
+  if(e.target.value.trim().length > 0) emailIsSet.email = true
+})
+
+function validateForm() {
+  const formValue = Object.values(emailIsSet)
+  const valid = formValue.findIndex(value => value == false);
+  if(valid == -1) form.submit()
+  else alert("Please enter your email")
+}
+
+/* CARD FORM */
+form.addEventListener("submit", sendCardInfo);
+
+function sendCardInfo(e) {
+  e.preventDefault();
+}
 
 
 visitor.addEventListener('click', () => {
