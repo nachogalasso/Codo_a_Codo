1206389cfa35048f8486fec1436c08e7ae34d340
nachogalasso <ignaciogalasso82@gmail.com>
2023-06-30 21:24:25 +0200

    node practice

---
+++ docs/node_sm_API/routes/auth.js
@@ -0,0 +1,10 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('Auth page')
+})
+
+module.exports = router
+
+// estoy en el minuto 14
+// https://www.youtube.com/watch?v=ldGl6L4Vktk&t=39s&ab_channel=LamaDev
